93ed1cd3185c3b22c69f08c8c7fbdd54e170ad7e
yuerwen <2660955327@qq.com>
2022-04-03 15:04:13 +0800

    Initial commit

--- auto_request/bysms_api_practice.py
+++ auto_request/bysms_api_practice.py
@@ -13,7 +13,7 @@ proxies = {
   'http': 'http://127.0.0.1:8888',
   'https': 'http://127.0.0.1:8888',
 }
-
+# 请求参数
 payload = {
 	'username': 'byhy',
 	'password': '88888888'
@@ -27,23 +27,28 @@ response = session.post('http://127.0.0.1/api/mgr/signin',
                         data=payload,
                         proxies = proxies
                         )
-responseData(response)
+# responseData(response)
 
+
+'''--------------------------------客户---------------------------'''
+# 列出所有客户
 # URL参数
 urlpara = {
 	'action' : 'list_customer',
-	'pagesize' : '4',
+	'pagesize' : '30',
 	'pagenum' : '1',
 	'keywords' : ''
 }
-
-# 列出所有客户
 response = session.get('http://127.0.0.1/api/mgr/customers',
                         params = urlpara,
                         proxies = proxies
                         )
 responseData(response)
 
+
+
+
+# 添加一个客户
 payload = {
     "action":"add_customer",
     "data":{
@@ -52,9 +57,145 @@ payload = {
         "address":"武汉市桥西医院北路"
     }
 }
-
-# 添加一个客户
 response = session.post('http://127.0.0.1/api/mgr/customers',
+                        json = payload,
+                        proxies = proxies
+                        )
+# responseData(response)
+
+
+
+# 修改客户信息
+payload = {
+    "action":"modify_customer",
+    "id": 695,
+    "newdata":{
+        "name":"武汉市桥北医院",
+        "phonenumber":"13345678888",
+        "address":"武汉市桥北医院北路"
+    }
+}
+response = session.put('http://127.0.0.1/api/mgr/customers',
+                        json = payload,
+                        proxies = proxies
+                        )
+# responseData(response)
+
+
+# 删除客户信息
+payload = {
+    "action":"del_customer",
+    "id": 696
+}
+
+response = session.delete('http://127.0.0.1/api/mgr/customers',
+                        json = payload,
+                        proxies = proxies
+                        )
+# responseData(response)
+
+
+'''--------------------------------药品---------------------------'''
+# 列出所有客户
+# URL参数
+urlpara = {
+	'action' : 'list_medicine',
+	'pagesize' : '4',
+	'pagenum' : '1',
+	'keywords' : ''
+}
+response = session.get('http://127.0.0.1/api/mgr/medicines',
+                        params = urlpara,
+                        proxies = proxies
+                        )
+responseData(response)
+
+
+# 添加一个药品
+payload = {
+    "action":"add_medicine",
+    "data":{
+        "name": "青霉素",
+        "desc": "青霉素 国字号",
+        "sn": "099877883837"
+    }
+}
+response = session.post('http://127.0.0.1/api/mgr/medicines',
+                        json = payload,
+                        proxies = proxies
+                        )
+# responseData(response)
+
+
+# 修改药品信息
+payload = {
+    "action":"modify_medicine",
+    "id": 279,
+    "newdata":{
+        "name": "修青霉素",
+        "desc": "修青霉素 国字号",
+        "sn": "0889898989898"
+    }
+}
+response = session.put('http://127.0.0.1/api/mgr/medicines',
+                        json = payload,
+                        proxies = proxies
+                        )
+# responseData(response)
+
+# 删除药品信息
+payload = {
+    "action":"del_medicine",
+    "id": 279
+}
+
+response = session.delete('http://127.0.0.1/api/mgr/medicines',
+                        json = payload,
+                        proxies = proxies
+                        )
+# responseData(response)
+
+
+'''--------------------------------订单---------------------------'''
+# 列出所有客户
+# URL参数
+urlpara = {
+	'action' : 'list_order',
+	'pagesize' : '4',
+	'pagenum' : '1',
+	'keywords' : ''
+}
+response = session.get('http://127.0.0.1/api/mgr/orders',
+                        params = urlpara,
+                        proxies = proxies
+                        )
+# responseData(response)
+
+# 添加一个订单
+payload = {
+    "action":"add_order",
+    "data":{
+        "name":"东莞理工学院3",
+        "customerid":691,
+        "medicinelist":[
+            {"id":290,"amount":5,"name":"青霉素"},
+            {"id":289,"amount":5,"name":"青霉素"}
+        ]
+    }
+}
+response = session.post('http://127.0.0.1/api/mgr/orders',
+                        json = payload,
+                        proxies = proxies
+                        )
+responseData(response)
+
+# 删除订单信息
+payload = {
+    "action":"delete_order",
+    "id": 291
+}
+
+response = session.delete('http://127.0.0.1/api/mgr/orders',
                         json = payload,
                         proxies = proxies
                         )
